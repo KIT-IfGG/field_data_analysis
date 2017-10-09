8b2bc26aa0cd8d722bc8bfa9ca9c4bc4e585778a
kdolos <dolos@kit.edu>
2017-10-09 16:02:34 +0200

    first vegetation data analysis added

--- r-code/vegetation_data_analysis.R
+++ r-code/vegetation_data_analysis.R
@@ -1,21 +1,37 @@
 library(vegan)
 library(isopam)
+
+mycols <- c("steelblue", "orange", "darkgreen", rainbow(5))
+
+
+### Read the data ###
 veg <- read.table("data/vegetation.csv", sep=",", dec=".", row.names = 1, header=TRUE)
 summary(veg)
 
 veg <- t(veg)
+
+### Ordination ####
 ordi <- metaMDS(veg)
 
 plot(ordi, type="n")
 orditorp(ordi, display="sites")
 orditorp(ordi, display="species", col="red")
 
+### Classification ####
 classif <- isopam(veg)
 isotab(classif)
 
-classif$flat$level.1
+classif <- isopam(veg, c.fix=3)
+isotab(classif)
 
+sink("results/isotab.txt")
+isotab(classif)
+sink()
 
+vclasses <- classif$flat
+
+### Plot classification results ####
 plot(ordi, type="n")
-orditorp(ordi, display="sites", col=classif$flat$level.1)
-#orditorp(ordi, display="species", col="red")
+orditorp(ordi, display="sites", col=mycols[vclasses])
+
+
