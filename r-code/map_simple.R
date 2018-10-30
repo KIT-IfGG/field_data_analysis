b5ed74621576eca28e383fc4ba63597354ae0a8a
kdolos <klara.dolos@gmail.com>
2018-10-30 15:59:13 +0100

    nicer map but still strange coordinates

--- r-code/map_simple.R
+++ r-code/map_simple.R
@@ -6,6 +6,7 @@ getwd()
 ### Read site data ####
 sites <- read.table("data/header_2015-2018.csv", sep=" ", header=TRUE)
 
+sites <- sites[!is.na(sites$longitude),]
 ### First plot
 sites$color <- 1 + sites$year - min(sites$year, na.rm = TRUE)
 mycolors <- c("blue", "red", "green", "black")
@@ -14,21 +15,30 @@ plot(latitude ~ longitude, data = sites, col = mycolors[sites$color], pch=16)
 ### Task: How many vegetation plots per year?
 ### Hint: table()
 
-### Plot with Shape file
-shp <- getData("GADM", country='DE', level=3, path = "../")
+### Plot with Shape file and DEM ####
+shp <- getData("GADM", country='DE', level=2, path = "../")
+dem <- getData("alt", country='DE', path = "../")
+coords <- SpatialPoints(sites[,c("longitude", "latitude")], CRS(projection(dem)))
 
-sregion <- shp[shp@data$NAME_1 %in% c("Rheinland-Pfalz", "Baden-Württemberg"),]
+buff <- 5000
+ka <- crop(shp, extent(buffer(coords, width=buff)))
+dem_ka <- crop(dem, extent(buffer(coords, width=buff)))
+labpt <- do.call(rbind, lapply(ka@polygons, function (x) x@labpt))
 
-ka <- sregion[sregion@data$NAME_2 %in% c("Karlsruhe", "Südliche Weinstraße"),]   #, "Germersheim", "Südwestpfalz", "Südliche Weinstraße"),]
 x11()
-plot(latitude ~ longitude, data = sites, col = mycolors[sites$color], pch=16, asp=1)
+plot(dem_ka, col=terrain.colors(10, alpha=0.7), xlab="Longitude", ylab="Latitude")
+points(latitude ~ longitude, data = sites, col = mycolors[sites$color], pch=16, asp=1)
 plot(ka, add=TRUE)
+text(labpt, ka@data$NAME_2, cex=0.7)
 
 ### TASK: Add a legend (year, colors) and write the map figure into a pdf file into a new folder "figures".
 ### Hints: legend(), pdf(), dev.off()
 
-pdf("results/test.pdf")
-plot(latitude ~ longitude, data = sites, col = mycolors[sites$color], pch=16, asp=1)
+pdf("figures/study_region.pdf", height = 7)
+plot(dem_ka, col=terrain.colors(10, alpha=0.7), xlab="Longitude", ylab="Latitude")
+points(latitude ~ longitude, data = sites, col = mycolors[sites$color], pch=16, cex=2)
+plot(ka, add=TRUE)
+text(labpt, ka@data$NAME_2, cex=0.7)
 legend("topright", legend = unique(sites$year), col = mycolors, pch = 16)
 dev.off()
 
